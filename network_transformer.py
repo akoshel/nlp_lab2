c134027f5bd931aeeb0e4787dd77219d573ef821
akoshel <johndoe@example.com>
2021-05-23 20:17:51 +0300

    batch first

--- network_transformer.py
+++ network_transformer.py
@@ -302,21 +302,6 @@ class Decoder(nn.Module):
 
         return output, attention
 
-    def forward(self, trg, enc_src, trg_mask, src_mask):
-        batch_size = trg.shape[0]
-        trg_len = trg.shape[1]
-
-        pos = torch.arange(0, trg_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
-
-        trg = self.dropout((self.tok_embedding(trg) * self.scale) + self.pos_embedding(pos))
-
-        for layer in self.layers:
-            trg, attention = layer(trg, enc_src, trg_mask, src_mask)
-
-        output = self.fc_out(trg)
-
-        return output, attention
-
 
 class Seq2Seq(nn.Module):
     def __init__(self,
@@ -403,5 +388,42 @@ def evaluate(model, iterator, criterion):
             loss = criterion(output, trg)
 
             epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
+
+
+def train(model, iterator, optimizer, criterion, clip, _, __):
+    model.train()
+
+    epoch_loss = 0
+
+    for i, batch in enumerate(iterator):
+        src = batch.src
+        trg = batch.trg
+
+        optimizer.zero_grad()
+
+        output, _ = model(src, trg[:, :-1])
+
+        # output = [batch size, trg len - 1, output dim]
+        # trg = [batch size, trg len]
+
+        output_dim = output.shape[-1]
+
+        output = output.contiguous().view(-1, output_dim)
+        trg = trg[:, 1:].contiguous().view(-1)
+
+        # output = [batch size * trg len - 1, output dim]
+        # trg = [batch size * trg len - 1]
+
+        loss = criterion(output, trg)
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
 
     return epoch_loss / len(iterator)
